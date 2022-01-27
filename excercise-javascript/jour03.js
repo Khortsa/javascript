a85beef6f90dc378ea55947a0dec0d75f4231c9a
Tenzin Choezom <khortsa2021@gmail.com>
2022-01-27 17:30:37 +0100

    excercise js 4

--- excercise-javascript/jour03.js
+++ excercise-javascript/jour03.js
@@ -1,2 +1,65 @@
+//excercise1//
 var fruits = ["mango", "lemon" , "bluberry"];
 console.log(fruits);
+console.table(fruits);
+
+//excercise2//
+let ingredients= ["eggs", "milk", "butter"];
+console.log(ingredients[1]);
+console.log(ingredients.indexOf('butter'));
+
+
+
+//excercise3//
+let objects = ["pen", "book", "lamp"];
+objects.unshift("chair");
+console.log(objects);
+objects.pop();
+console.log(objects);
+objects.push("laptop");
+console.log(objects);
+objects.shift();
+console.log(objects);
+
+//exercise4//
+let numbers = [4, 10, 8, 12, 6];
+numbers.reverse();
+console.log(numbers);
+numbers.sort();
+console.log(numbers);
+//excercise5//
+let total = 0;
+let limit = 10;
+ for (var i = 0; i <= limit; i++){
+    total = total + i;
+    console.log(total);
+ }
+
+ //excercise6//
+ let sentence = "Hello Konexio";
+ for (let i = sentence.length -1; i >=0; i --){
+     console.log(sentence[i]);
+ }
+
+ //Bonus1//
+ for (let i = 0; i <=100; i++)
+ if(i % 3 === 0){
+     console.log('fizz');
+ } else if (i % 5 === 0) {
+     console.log('buzz');
+ } else if(i % 3 && i % 5 === 0){
+     console.log('fizzbuzz');
+ } else if(i % 7 === 0) {
+     console.log();
+ } else{
+    console.log(i);
+ }
+ 
+ //Bonus2//
+ let total = 0;
+ let limit = 10;
+
+ while (total < limit) {
+	total++;
+    console.log(total)
+}
